b7a1a26330056ddbf13d5833663c76eac9abdf94
nabievanush <nabievanush1@gmail.com>
2021-11-26 22:42:09 +0300

    first try

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,33 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var result string
+	for i, c := range s {
+		n, err := strconv.Atoi(string(c))
+		if i != 0 {
+			if err != nil {
+				if n != 0 {
+					result += strings.Repeat(string(s[i-1]), n-1)
+				} else {
+					result = result[:i-1]
+				}
+			} else {
+				result += string(s[i])
+			}
+		} else {
+			if err != nil {
+				result += string(s[i])
+			} else {
+				return ErrInvalidString.Error(), ErrInvalidString
+			}
+		}
+	}
+	return result, nil
 }
