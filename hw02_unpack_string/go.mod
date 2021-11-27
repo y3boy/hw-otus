540809511c2eb370bf7f54707a55a0b9df66277d
nabievanush <nabievanush1@gmail.com>
2021-11-27 20:38:19 +0300

    Done

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -3,9 +3,9 @@ module github.com/fixme_my_friend/hw02_unpack_string
 go 1.16
 
 require (
-	github.com/cpuguy83/go-md2man v1.0.10 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-delve/delve v1.7.3 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/stretchr/testify v1.7.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
@@ -11,22 +12,19 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(s string) (string, error) {
 	var result string
 	for i, c := range s {
-		n, err := strconv.Atoi(string(c))
-		if i != 0 {
-			if err != nil {
-				if n != 0 {
-					result += strings.Repeat(string(s[i-1]), n-1)
-				} else {
-					result = result[:i-1]
-				}
+		n, _ := strconv.Atoi(string(c))
+		switch {
+		case !(unicode.IsDigit(c)): // Если символ буква
+			result += string(c)
+		case i == 0 && unicode.IsDigit(c): // Первый символ цифра
+			return ErrInvalidString.Error(), ErrInvalidString
+		case unicode.IsDigit(rune(s[i+1])): // Цифра часть числа
+			return ErrInvalidString.Error(), ErrInvalidString
+		default: // Если цифра
+			if n == 0 {
+				result = result[:len(result)-1]
 			} else {
-				result += string(s[i])
-			}
-		} else {
-			if err != nil {
-				result += string(s[i])
-			} else {
-				return ErrInvalidString.Error(), ErrInvalidString
+				result += strings.Repeat(string(s[i-1]), n-1)
 			}
 		}
 	}
